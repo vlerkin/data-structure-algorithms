1ff824645075461c757db01aebcc223f50a08e41
Valeriia Klestova <valeriya.apukhtina@gmail.com>
2023-08-19 12:50:04 +0200

    added swap function to perform item swapping for linked lists

--- linear-data-str.py
+++ linear-data-str.py
@@ -57,6 +57,48 @@ class LinkedList:
                 else:
                     current_node = next_node
 
+def swap_nodes(input_list, val1, val2):
+    print(f'Swapping {val1} with {val2}')
+
+    node1_prev = None
+    node2_prev = None
+    node1 = input_list.head_node
+    node2 = input_list.head_node
+
+    if val1 == val2:
+        print("Elements are the same - no swap needed")
+        return
+
+    while node1 is not None:
+        if node1.get_value() == val1:
+            break
+        node1_prev = node1
+        node1 = node1.get_next_node()
+
+    while node2 is not None:
+        if node2.get_value() == val2:
+            break
+        node2_prev = node2
+        node2 = node2.get_next_node()
+
+    if (node1 is None or node2 is None):
+        print("Swap not possible - one or more element is not in the list")
+        return
+
+    if node1_prev is None:
+        input_list.head_node = node2
+    else:
+        node1_prev.set_next_node(node2)
+
+    if node2_prev is None:
+        input_list.head_node = node1
+    else:
+        node2_prev.set_next_node(node1)
+
+    temp = node1.get_next_node()
+    node1.set_next_node(node2.get_next_node())
+    node2.set_next_node(temp)
+
 # Test
 ll = LinkedList(5)
 ll.insert_beginning(70)
@@ -64,6 +106,14 @@ ll.insert_beginning(5675)
 ll.insert_beginning(90)
 my_node = LinkedList(None)
 my_node.remove_node(None)
+ll = LinkedList()
+for i in range(10):
+    ll.insert_beginning(i)
+
+
+print(ll.stringify_list())
+swap_nodes(ll, 9, 5)
+print(ll.stringify_list())
 
 
 class DoublyLinkedList:
@@ -164,6 +214,8 @@ class DoublyLinkedList:
             string_list += str(current_node.get_value()) + "\n"
         current_node = current_node.get_next_node()
     return string_list
+
+
 
 subway = DoublyLinkedList()
 subway.add_to_head("Times Square")
